3188282306da4df0d679c00ef4293b9ee2b5b849
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-01-27 22:07:20 -0500

    added jQuery variables script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-
+let firstName = $('#firstName');
+let lastName = $('#lastName');
+let monthlyIncome = $('#monthlyIncome');
+let userLocation = $('#userLocation');
+let expense = $('#expense');
+let cost = $('#cost');
+let categories = $('#categories');
 
