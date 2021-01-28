7138b61b193fd9253b4647b66c2345e78e859199
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-01-28 13:31:13 -0500

    added Id to col, added class hidden, made click event for submit

--- script.js
+++ script.js
@@ -5,4 +5,22 @@ let userLocation = $('#userLocation');
 let expense = $('#expense');
 let cost = $('#cost');
 let categories = $('#categories');
+let budgetTarget = $('#budget');
+
+let budget;
+let locations;
+
+$('#form-1-submit').on('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (monthlyIncome.val() && userLocation.val()) {
+        $('#col-1').addClass('hidden');
+        $('#col-2').removeClass('hidden');
+        budget = monthlyIncome.val();
+        locations = userLocation.val();
+        budgetTarget.html(budget);
+        console.log(budget)
+    }
+});
+
 
