a6a4a821f588267374e25308d297a931c548336c
FrankOrtega <fortega@envios2224.com>
2021-02-02 18:44:27 -0500

    updated multicity and embed map

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let budgetTarget = $("#budget");
 
 let budget;
 let locations;
+let restaurants = {};
 
 $("#form-1-submit").click(function (event) {
     event.preventDefault();
@@ -21,6 +22,7 @@ $("#form-1-submit").click(function (event) {
         locations = userLocation.val();
         //Sets the budget html to be equal to their monthly budget fixed to 2 decimal places.
         budgetTarget.html(`$${(parseInt(budget)).toFixed(2)}`);
+        $("#requestRest").prop("disabled", false);
     }
 });
 
@@ -32,12 +34,56 @@ $("#form-2-submit").click(function (event) {
         let expenseCost = $("#cost").val();
         let expenseList = $("#listOfExpenses");
 
+        let firstLetter = expenseName[0].toUpperCase();
+        //Creates new string with capital letter.
+        let newName = "";
+        newName += firstLetter;
+        for (let i = 1; i < expenseName.length; i++) {
+            newName += expenseName[i];
+        }
+
+        console.log(newName);
+
+        let div = $("<div>");
+        let span = $("<span>");
         let listItem = $("<li>");
-        listItem.html(`${expenseName}: ${expenseCost}`);
-        expenseList.append(listItem);
+        let button = $("<span>");
+
+        listItem.html(`${newName}: ${expenseCost}`);
+        listItem.addClass("list-group-item text-dark float-left");
+        button.html("<i class='fa fa-trash'></i>");
+        button.addClass("ml-2 float-right");
+
+        listItem.append(button);
+        span.append(listItem);
+        div.append(span);
+        expenseList.append(div);
 
         //Clear inputs
         $("#expense").val('');
         $("#cost").val('');
+        //Subtracts expense cost from budget and reupdates html
+        budget -= parseFloat(expenseCost);
+        console.log(budget);
+        budgetTarget.html(`$${(parseFloat(budget)).toFixed(2)}`)
     }
+});
+
+$("#requestRest").on("click", function () {
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search"
+
+    $.ajax({
+        url: myurl,
+        headers: {
+            'Authorization': 'Bearer L-II2r_Slet4z_EkoQ8O0wf3dRrb_tgQE2q81nmfYx5qT-TnC_Lox30a4ztshh-4S2e9bf7imSQ-dxWdjFXKW9vYQvqV6gLTYb1mCSP9gj4282zbST2TlLbJtJsNYHYx',
+        },
+        method: 'GET',
+        data: { term: 'restaurant', location: locations, limit: '5', price: '1' },
+    }).then(function (response) {
+        console.log(response);
+        for (let i = 0; i < response.businesses.length; i++) {
+            restaurants[response.businesses[i].name] = response.businesses[i].coordinates;
+        }
+        console.log(restaurants);
+    });
 });
